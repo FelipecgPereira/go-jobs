91908817eec579b81339281ca513a172af9e79f0
FelipecgPereira <felipepereiracg@gmail.com>
2026-06-28 15:02:09 -0300

    implement user authentication with JWT and add validation for credentials

--- api/models/user.go
+++ api/models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"errors"
 	"time"
 
 	"com.github/FelipecgPereira/go-jobs/db"
@@ -14,6 +15,26 @@ type User struct {
 	Password string `binding:"required`
 	createAt time.Time
 	updateAt time.Time
+}
+
+func (input *User) ValidateCredentials() error {
+	query := "SELECT id, password FROM users where  email= ?"
+	row := db.DB.QueryRow(query, input.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&input.Id, &retrievedPassword)
+
+	if err != nil {
+		return errors.New("Credentials invalid")
+	}
+
+	isValidPassword := utils.CheckPassword(input.Password, retrievedPassword)
+
+	if !isValidPassword {
+		return errors.New("Credentials invalid")
+	}
+
+	return nil
 }
 
 func (input *User) Save() error {
--- api/routes/user.route.go
+++ api/routes/user.route.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"com.github/FelipecgPereira/go-jobs/models"
+	"com.github/FelipecgPereira/go-jobs/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,5 +25,31 @@ func signup(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+
+}
+
+func auth(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindBodyWithJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request body"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not athenticate user"})
+	}
+
+	token, err := utils.GenerateToken(user.Email, user.Id)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful", "token": token})
 
 }
